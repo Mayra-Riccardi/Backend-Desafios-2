b36741f068b851d5f184fdd86fcd99242baf10e9
Mayra-Riccardi <may_ricca5@hotmail.com>
2023-02-03 03:06:08 -0300

    servidor express en capas

--- DESAFIO 16/src/config/env.config.js
+++ DESAFIO 16/src/config/env.config.js
@@ -1,13 +1,10 @@
 const path = require('path');
 const dotenv = require('dotenv');
 
-//configuracion del config// NODE_ENV = 'development || production
-dotenv.config({
-    path: path.resolve(process.cwd(), `${process.env.NODE_ENV || 'development'}.env`)
-})
+dotenv.config();
+
 
 const config = {
-    NODE_ENV: process.env.NODE_ENV || 'development',
     HOST: process.env.HOST || 'localhost',
     PORT: process.env.PORT || 8080,
     SESSION_SECRET: process.env.SESSION_SECRET || '',
